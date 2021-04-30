396d5d0cd9677e7a39b2c534ad76a6c753b7ee3e
snoozy-fox <lancasja@gmail.com>
2021-04-29 23:06:11 -0700

    Sets up linktree-like basics

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,33 +1,76 @@
 import React from "react"
 import SEO from "../components/seo"
-import { Global, css } from "@emotion/core"
+import { Global } from "@emotion/core"
 import { ThemeProvider } from "emotion-theming"
-import { Box } from "rebass"
+import { Flex } from "rebass"
 
 const theme = {
-
+  colors: {
+    dark: "#111",
+    gray: "#333",
+    light: "#eee"
+  }
 }
 
 const IndexPage = () => (
   <ThemeProvider theme={ theme }>
     <SEO title="Home" />
     <Global
-      styles={css`
-        body {
-          margin: 0;
-          font-family: sans-serif;
+      styles={{
+        body: {
+          margin: 0,
+          fontFamily: "sans-serif"
         }
-      `}
+      }}
     />
-    <Box
+    <Flex
       sx={{
         p: 40,
-        bg: "#111",
-        color: "#eee"
+        bg: "gray",
+        color: "light",
+        height: "100vh",
+        alignItems: "center",
+        justifyContent: "center"
       }}
     >
-      Hi, I'm Jonathan, this is going to be my site.
-    </Box>
+      <Flex
+        sx={{
+          p: 4,
+          position: "fixed",
+          top: 0,
+          bg: "dark",
+          color: "light",
+          width: "100%",
+          justifyContent: "flex-end"
+        }}
+      >
+        We make music
+      </Flex>
+      <Flex
+        sx={{
+          bg: "dark",
+          p: 4,
+          borderRadius: 8,
+          border: "1px solid ",
+          letterSpacing: "1px",
+          flex: 1
+        }}
+      >
+        Check it out here
+      </Flex>
+    </Flex>
+    <Flex
+      sx={{
+        p: 4,
+        position: "fixed",
+        bottom: 0,
+        bg: "dark",
+        color: "light",
+        width: "100%"
+      }}
+    >
+      And connect with us here
+    </Flex>
   </ThemeProvider>
 )
 
